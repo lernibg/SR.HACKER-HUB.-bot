c270fc61cc125403dc113bd5df975121965199cb
lernibg <44763315+lernibg@users.noreply.github.com>
2018-11-06 14:26:13 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,29 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+var Discord = require('discord.js');
+var botName = require('./package.json');
+var bot = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+bot.on('message', message =>{
+
+    // Variables
+    var sender = message.author; //The person who sent the message
+    var msg = message.content;
+    var prefix = ">" //The text before commands, you can put anything that you prefer
+
+    if(message.author.id != "508868117661745153" && message.channel.id === "508219003685175311"){
+        if(msg.startsWith('>', 0)){
+            if(msg === prefix + "bang" && message.channel.id === "508219003685175311"){
+                message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
+            }
+            else{
+                message.channel.send("Hey, I only understand >bang right now. More will be added when my creator is free. :) ")
+            }
+        }
+        else{
+            message.delete();
+            message.channel.send(sender + " this bot only accepts commands which starts with '>' dot.");
+        }
+    }
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+bot.login(process.env.BOT_TOKEN)
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
